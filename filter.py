6717f8875ab18ab84290434d70580a852e97a333
KirillDmit <kirilldmitriev20015@gmail.com>
2021-11-25 17:58:08 +0500

    Update filter.py
    add doctests

--- filter.py
+++ filter.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 def convert_image_to_mosaic(image, size, gradation_step):
+    """
+    :param image:
+    :param size:
+    :param gradation_step:
+    :return image:
+    """
+
     for x in range(0, len(image), size):
         for y in range(0, len(image[0]), size):
             image[x:x + size, y:y + size] = get_average_brightness(
@@ -11,6 +18,19 @@ def convert_image_to_mosaic(image, size, gradation_step):
 
 
 def get_average_brightness(block, size, gradation_step):
+    """
+    Возвращает среднюю яркость с оттенками серого
+    :param block:
+    :param size:
+    :param gradation_step:
+    :return int:
+
+    >>> get_average_brightness(np.array([[[1, 2, 3], [4, 5, 6]]]), 50, 5)
+    0
+
+    >>> get_average_brightness(np.array([[[15, 10, 11]], [[40, 11, 15]]]), 150, 10)
+    0
+    """
     average_color = (block[:size, :size].sum() / 3) // size ** 2
     return int(average_color // gradation_step) * gradation_step
 
